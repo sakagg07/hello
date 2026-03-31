c6ae3f4f48e02d6d6df9b8bd57f6a01e6e370c87
Sakshi Agrawal <sak07.agg@gmail.com>
2026-03-31 20:22:16 +0530

    Added HelloApp.java

--- src/HelloApp/HelloApp.java
+++ src/HelloApp/HelloApp.java
@@ -1,13 +1,22 @@
 /**
- * HelloApp - UC1 - Display Hello World
+ * HelloApp - UC2 - Display Hello with Command-Line Argument
  * @author Adit
- * @version 1.0
+ * @version 2.0
  */
 public class HelloApp {
     public static void main(String[] args) {
         
-        // Display Hello World to console
-        System.out.println("Hello World");
+        // Check if user provided a name as command-line argument
+        if (args.length > 0) {
+            // Get the name from command-line argument
+            String name = args[0];
+            
+            // Display personalized greeting
+            System.out.println("Hello, " + name);
+        } else {
+            // No argument provided, display default
+            System.out.println("Hello World");
+        }
         
     }
 }
