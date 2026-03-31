59ad1ac171a33f979abff4861e6ece2fc91c5d73
Sakshi Agrawal <sak07.agg@gmail.com>
2026-03-31 19:59:28 +0530

    Initial commit

---
+++ src/HelloApp/HelloApp.java
@@ -0,0 +1,13 @@
+/**
+ * HelloApp - UC1 - Display Hello World
+ * @author Adit
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        
+        // Display Hello World to console
+        System.out.println("Hello World");
+        
+    }
+}
